47b79551843abce22937540c3d1062290011888d
myomi <myomi@shibainu>
2013-10-22 00:42:43 +0900

    add q01-06

---
+++ src/works/work001/Main001_06.java
@@ -0,0 +1,11 @@
+package works.work001;
+
+/**
+ * さいころの動作確認クラス
+ * @author you!
+ */
+public class Main001_06 {
+	public static void main(String[] args) {
+		// TODO 問題01-06をやってみよう「勝ち負け判定ゲームver3を作リ、サワムラを黙らせろ！」
+	}
+}
