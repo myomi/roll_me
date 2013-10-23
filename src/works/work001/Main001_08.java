03dd42d5c7b5c663a5821491d99b0e619b61b48d
myomi <myomi@shibainu>
2013-10-24 00:14:58 +0900

    add q01-08

---
+++ src/works/work001/Main001_08.java
@@ -0,0 +1,25 @@
+package works.work001;
+
+import ors.dice.Dice;
+
+/**
+ * さいころの動作確認クラス
+ * @author you!
+ */
+public class Main001_08 {
+	public static void main(String[] args) {
+		// TODO 問題01-08をやってみよう「さいころてんこ盛りゲームver2を作れ！」
+	}
+
+	/**
+	 * 指定された回数だけさいころを振り、出た目の合計を返します
+	 * @param dice さいころ
+	 * @param times さいころを振る回数
+	 *
+	 * @return さいころの目の合計
+	 */
+	public static long rollTimes(Dice dice, int times) {
+		// TODO 問題01-08をやってみよう「roleTimes メソッドを実装しろ！」
+		return 0;
+	}
+}
