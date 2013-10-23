d6624ddd96cfbdab3e50e3d23d8211fbc45ed0cd
myomi <myomi@shibainu>
2013-10-23 23:48:54 +0900

    add q01-07

---
+++ src/works/work001/Main001_07.java
@@ -0,0 +1,11 @@
+package works.work001;
+
+/**
+ * さいころの動作確認クラス
+ * @author you!
+ */
+public class Main001_07 {
+	public static void main(String[] args) {
+		// TODO 問題01-07をやってみよう「さいころてんこ盛りゲームver1を作れ！」
+	}
+}
